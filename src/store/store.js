927b2edb8d0d3a1050295aa0fbc687f5f4b75ec4
Yungun Lee <yungunpatron@gmail.com>
2018-10-05 22:32:03 -0400

    Install dev dependencies

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import reducers from './reducers'
+
+export const store = createStore(
+    reducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(
+        thunkMiddleware,
+    )
+)
